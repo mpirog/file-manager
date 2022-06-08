2968f7e0717610670c392ea2a10f3ce42ce1b4b4
imychko <imychko@lumentouch.com>
2022-06-08 13:00:05 +0300

    feat: implement user definition function

---
+++ src/user.js
@@ -0,0 +1,17 @@
+export const getUserName = () => {  
+  let userName = null;
+
+  const args = process.argv.slice(2).forEach((value) => {
+    const val = value.split('=');
+
+    if (val[0] === '--username') {
+      userName = val[1];
+    }
+  });
+
+  if (!userName && typeof userName !== 'number') {
+    throw new Error('User name was not defined')
+  }
+
+  return userName;
+};
