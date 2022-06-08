6fb8c7f901809dff51da8e5d9ca5d1dcb15645ed
imychko <imychko@lumentouch.com>
2022-06-08 13:07:16 +0300

    feat: implement the factory command class

---
+++ src/CommandFactory.js
@@ -0,0 +1,34 @@
+import up from "./up.js";
+
+class CommandFactory {
+  constructor(param) {
+    const params = param.split(' ');
+    this._command = params[0];
+    this._args = params.splice(1);
+  }
+
+  getInstance() {
+    switch (this._command) {
+      case 'up':
+        return new up(this._args);
+
+      case 'cd':
+      case 'ls':
+      case 'cat':
+      case 'add':
+      case 'rn':
+      case 'cp':
+      case 'mv':
+      case 'rm':
+      case 'os':
+      case 'hash':
+      case 'compress':
+      case 'decompress':
+
+      default:
+        throw new Error('Command was no defined')
+    }
+  }
+}
+
+export default Collector;
